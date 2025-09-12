847832dd045fc0284fc473a1efe1cf7b70957f2f
Smitapatil65 <smitap050265@gmail.com>
2025-09-12 07:44:47 +0530

    updated sumWith0.cpp with new logic # On branch main

--- sumWith0.cpp
+++ sumWith0.cpp
@@ -1,10 +1,51 @@
 #include<iostream>
-#include<climits>
-#include<math.h>
+#include<unordered_map>
 #include<vector>
 
 using namespace std;
 
+bool sum(vector<int>&a,int &target)
+{
+    int n=a.size();
+    unordered_map<int,int>mp;
+
+    for(auto l:a)
+    {
+        mp[l]++;
+    }
+
+    for(auto i=mp.begin();i!=mp.end();i++)
+    {
+        int key=i->first;
+        int val=i->second;
+
+        int pair=target-key;
+
+        if(key==pair)
+        {
+            if(val>1)
+                return true;
+        }else{
+          if(mp.find(pair)!=mp.end())
+            return true;
+        }
+    }
+    return false;
+}
+int main()
+{
+    vector<int>a={4, 2, -3, 1, 6};
+    int t=11;
+    if(sum(a,t))
+        cout<<"True";
+    else
+        cout<<"False";
+
+   return true;
+
+
+
+}
 
 
 
