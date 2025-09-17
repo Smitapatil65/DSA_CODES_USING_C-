9ce445d404829417d0546b7049386785393fa5bb
Smitapatil65 <smitap050265@gmail.com>
2025-09-17 18:45:30 +0530

    My new Programs Added

---
+++ Election.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+
+using namespace std;
+
+vector<string,int> election(vector<string>&a)
+{
+    int n=a.size();
+    unordered_map<string,int>mp;
+
+
+    for(auto i:a)
+    {
+        mp[a]++;
+    }
+
+
+
+
+}
