52561925664c0c0ca5594b34c9fd42e0656b2877
Smitapatil65 <smitap050265@gmail.com>
2025-12-01 08:35:18 +0530

    Added new code

---
+++ JOsep.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<deque>
+#include<math.h>
+#include<vector>
+
+using namespace std;
+
+int josh(int n,int k)
+{
+
+    deque<int> q;
+    for(int i=1;i<=n;i++)
+    {
+        q.push_back(i);
+    }
+
+    while(q.size()>1)
+    {
+        for(int i=1;i<k;i++)
+        {
+            q.push_back(q.front());
+            q.pop_front();
+        }
+        q.pop_front();
+    }
+    return q.front();
+}
+int main()
+{
+    int n=5,k=2;
+    cout<<"Ans="<<josh(n,k);
+    return 0;
+}
+
+
+
